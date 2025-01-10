63dd8b11bc4dcd7f302cdd064e8d7b5134cae2a0
kdjadeja21 <krushnasinh.jadeja14545@marwadieducation.edu.in>
2025-01-10 14:20:02 +0530

    Add Analytics to the web app.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         {children}
+        <Analytics />
         <Toaster position="top-center" />
         <footer className="py-4 text-center text-gray-600">
           Made with ❤️ in India By{" "}
